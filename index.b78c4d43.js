d9fb58b39250d2d9870bb17a6856b7d00823c035
Voloshyn9900 <110063136+Voloshyn9900@users.noreply.github.com>
2023-09-10 19:33:22 +0000

    Deploying to gh-pages from @ Voloshyn9900/orderQR@db5095499a563bc33199d76dd1d204bbb5d0e390 🚀

--- index.b78c4d43.js
+++
@@ -1,2 +0,0 @@
-console.log("9",fetch("".concat("http://localhost:3030","/orders")).then((function(o){return o.json()})).then(console.log)),console.log("orderQR");
-//# sourceMappingURL=index.b78c4d43.js.map
--- index.e486ea09.js
+++
@@ -1,2 +0,0 @@
-console.log("9",fetch("http://localhost:3030/orders").then((o=>o.json())).then(console.log)),console.log("orderQR");
-//# sourceMappingURL=index.e486ea09.js.map
